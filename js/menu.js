65f1e179146729864d987fc3b6cc1a53c9945232
Andrii55 <Homzykandriy@gmail.com>
2023-01-06 13:04:57 +0200

    homework-8.10.

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,21 @@
 (() => {
-    const refs = {
-      openMenuBtn: document.querySelector(".header__btn__open"),
-      closeMenuBtn: document.querySelector(".header__btn__close"),
-      menu: document.querySelector(".header__mob__menu"),
-    };
-  
-    refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
-  
-    function toggleMenu() {
-      refs.menu.classList.toggle("is-hidden");
-    }
-  })();
+  const mobileMenu = document.querySelector('.header__mob__menu');
+  const openMenuBtn = document.querySelector('.header__btn__open');
+  const closeMenuBtn = document.querySelector('.header__btn__close');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+})();
